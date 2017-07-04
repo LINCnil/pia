55c0b9f843e5408829d576fd8d10c988747be802
patrick <patrick@atnos.com>
2017-07-04 16:59:09 +0200

    adds attachment items + misc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,18 @@ import { CardItemComponent } from './cards/card-item/card-item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 import { EntryComponent } from './entry/entry.component';
-import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
+import { SectionsComponent } from './entry/sections/sections.component';
+import { AttachmentsComponent } from './entry/attachments/attachments.component';
+import { EntryContentComponent } from './entry/entry-content/entry-content.component';
+import { KnowledgeBaseComponent } from './entry/knowledge-base/knowledge-base.component';
+import { KnowledgeBaseFiltersComponent } from './entry/knowledge-base/knowledge-base-filters/knowledge-base-filters.component';
+import { KnowledgeBaseItemComponent } from './entry/knowledge-base/knowledge-base-item/knowledge-base-item.component';
+import { KnowledgeBaseSearchComponent } from './entry/knowledge-base/knowledge-base-search/knowledge-base-search.component';
+import { AttachmentItemComponent } from './entry/attachments/attachment-item/attachment-item.component';
+import { ModalsComponent } from './modals/modals.component';
+
+
 
 const appRoutes: Routes = [
   { path: '', component: AuthenticationComponent },
@@ -28,9 +37,17 @@ const appRoutes: Routes = [
     CardsComponent,
     FiltersComponent,
     CardItemComponent,
-    SidebarRightComponent,
     EntryComponent,
-    SidebarLeftComponent
+    SectionsComponent,
+    AttachmentsComponent,
+    EntryContentComponent,
+    KnowledgeBaseComponent,
+    KnowledgeBaseFiltersComponent,
+    KnowledgeBaseItemComponent,
+    KnowledgeBaseSearchComponent,
+    AttachmentItemComponent,
+    ModalsComponent,
+
   ],
   imports: [
     BrowserModule,
