5e1459499867492a165bc72c604f6b3c5f3c5d9f
patrick <patrick@atnos.com>
2017-07-04 17:00:05 +0200

    Adds attachment item (for real)

---
+++ src/app/entry/attachments/attachment-item/attachment-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attachment-item',
+  templateUrl: './attachment-item.component.html',
+  styleUrls: ['./attachment-item.component.scss']
+})
+export class AttachmentItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
