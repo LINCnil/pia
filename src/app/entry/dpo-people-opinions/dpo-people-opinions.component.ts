dc3eab4fe8a8c16294fe1df7e34e8136c24dc1df
patrick <patrick@atnos.com>
2017-07-10 17:43:01 +0200

    add dpo and put checked and disabled status = pending

--- src/app/entry/dpo-people-opinions/dpo-people-opinions.component.ts
+++ src/app/entry/dpo-people-opinions/dpo-people-opinions.component.ts
@@ -7,14 +7,28 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./dpo-people-opinions.component.scss']
 })
 export class DPOPeopleOpinionsComponent implements OnInit {
-  treatments = ['Le traitement peut être mis en oeuvre', 'Le traitement ne doit pas être mis en oeuvre'];
   treatmentForm: FormGroup;
+  dpoAvisForm: FormGroup;
+  public dpoEditStatus: boolean;
+  public peopleEditStatus: boolean;
   constructor() { }
+
+  /**
+   * Disables fields and save data.
+   */
+  dpoFocusOut() {
+    this.dpoEditStatus = true;
+    // Saving data here
+    console.log('ok');
+  }
+
 
   ngOnInit() {
     this.treatmentForm = new FormGroup({
       treatment : new FormControl()
     });
+    this.dpoAvisForm = new FormGroup({
+      dpoAvis : new FormControl()
+    });
   }
-
 }
