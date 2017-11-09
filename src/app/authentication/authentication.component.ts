a99bdc75f842e3e7f17cfbbf829f40e46a820618
Kevin Beyrand <kevin@atnos.com>
2017-11-09 17:31:26 +0100

    trello#186 updates homepage text + trello#195 adds languages switch button on homepage

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,26 +1,45 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Renderer2 } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-authentication',
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.scss']
 })
-export class AuthenticationComponent implements OnInit, OnDestroy {
+export class AuthenticationComponent implements OnInit, DoCheck {
+  selectedLanguage: string;
 
-  constructor(private renderer: Renderer2) {
-    this.renderer.addClass(document.body, 'pia-authentication');
-  }
+  constructor(private _translateService: TranslateService) { }
 
   ngOnInit() {
+    this.getUserLanguage();
+  }
+
+  ngDoCheck() {
+    this.getUserLanguage();
   }
 
   /**
-   * Removes authentication class on component destruction, so that it is not shown on other pages.
-   * @memberof AuthenticationComponent
+   * Record the selected language
+   * @param {string} selectedLanguage
+   * @memberof HeaderComponent
    */
-  ngOnDestroy() {
-    this.renderer.removeClass(document.body, 'pia-authentication');
+  updateCurrentLanguage(selectedLanguage: string) {
+    localStorage.setItem('userLanguage', selectedLanguage);
   }
 
+  /**
+   * Retrieve the selected language
+   * @memberof HeaderComponent
+   */
+  getUserLanguage() {
+    const language = localStorage.getItem('userLanguage');
+    if (language && language.length > 0) {
+      this.selectedLanguage = language;
+    } else {
+      const browserLang = this._translateService.getBrowserLang();
+      this.selectedLanguage = browserLang.match(/en|fr/) ? browserLang : 'fr';
+    }
+  }
 }
