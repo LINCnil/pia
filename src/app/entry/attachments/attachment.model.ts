b4fe87c2eedcc16ff1f8c38d2ee65ce6e0480190
Bruno Perles <bruno@atnos.com>
2018-02-23 08:59:17 +0100

    Add comment on removed attachment

--- src/app/entry/attachments/attachment.model.ts
+++ src/app/entry/attachments/attachment.model.ts
@@ -6,7 +6,7 @@ export class Attachment extends ApplicationDb {
   public name: string;
   public mime_type: string;
   public pia_signed = 0;
-  public history_comment: string;
+  public comment: string;
 
   constructor() {
     super(201708291502, 'attachment');
@@ -20,7 +20,7 @@ export class Attachment extends ApplicationDb {
           pia_id: this.pia_id,
           pia_signed: this.pia_signed,
           file: this.file,
-          history_comment: this.history_comment,
+          comment: this.comment,
           created_at: this.created_at
         };
     await this.getObjectStore();
@@ -53,6 +53,46 @@ export class Attachment extends ApplicationDb {
           reject(Error(event));
         }
       }
+    });
+  }
+
+  async remove(comment: string) {
+    return new Promise((resolve, reject) => {
+      this.find(this.id).then((entry: any) => {
+        entry.file = null;
+        entry.comment = comment;
+        entry.updated_at = new Date();
+        if (this.serverUrl) {
+          const formData = new FormData();
+          for (const d in entry) {
+            if (entry.hasOwnProperty(d)) {
+              formData.append('attachment[' + d + ']', entry[d]);
+            }
+          }
+          fetch(this.getServerUrl() + '/' + entry.id, {
+            method: 'PATCH',
+            body: formData
+          }).then((response) => {
+            return response.json();
+          }).then((result: any) => {
+            resolve();
+          }).catch ((error) => {
+            console.error('Request failed', error);
+            reject();
+          });
+        } else {
+          this.getObjectStore().then(() => {
+            const evt = this.objectStore.put(entry);
+            evt.onerror = (event: any) => {
+              console.error(event);
+              reject(Error(event));
+            }
+            evt.onsuccess = () => {
+              resolve();
+            };
+          });
+        }
+      });
     });
   }
 
