7d1885a67f3248a6f669a16902d0ccfce0cb19e3
patrick <patrick@atnos.com>
2018-02-23 18:20:23 +0100

    add help page and nav on left

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit } from '@angular/core';
 import { MarkdownService } from 'angular2-markdown';
 import {Http} from '@angular/http';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-help',
@@ -8,17 +9,19 @@ import {Http} from '@angular/http';
   styleUrls: ['./help.component.scss']
 })
 export class HelpComponent implements OnInit {
-  private titleLists = [];
-  private subtitleLists = [];
+
+  private tableOfTitles = [];
   private currentAnchorId: string
-  constructor(private http: Http, private _markdown: MarkdownService) {  }
+  public activeElement: string;
+  constructor(private http: Http, 
+              private _markdown: MarkdownService, 
+              private _translateService: TranslateService)
+              {  }
 
   ngOnInit() {
     this._markdown.renderer.blockquote = (quote: string) => {
       return `<blockquote class="king-quote">${quote}</blockquote>`;
     };
-    this.getSectionList();
-
     window.onscroll = function(ev) {
       if (window.innerWidth > 640) {
         const el: any = document.querySelector('.pia-help-section');
@@ -33,34 +36,47 @@ export class HelpComponent implements OnInit {
         }
       }
     };
+    this.getSectionList();
+  }
+  getAnchor(event, text) {
+    event.preventDefault();
+    this.activeElement = text;
+    const cleanTarget = text.trim().toLowerCase().replace(/\s/g, '-');
+    document.getElementById(cleanTarget).scrollIntoView();
   }
 
   getSectionList() {
     return new Promise(resolve => {
-      this.http.get('assets/files/pia_help_fr.md').subscribe(data => {
+      this.http.get(`assets/files/pia_help_fr.md`).subscribe(data => {
         const fileMd = data.text().toString();
-        const reg1 = new RegExp('^(##) [A-Za-z 1-9]+', 'gm');
-        const reg2 = new RegExp('^(###) [A-Za-z 1-9]+', 'gm');
-        const titleLists = this.titleLists;
-        fileMd.match(reg1).forEach(function (titleH2) {
-          titleLists.push(titleH2.toString().replace('##', ''))
+        const lines = fileMd.split('\n');
+        let tt = [];
+        lines.forEach((line) => {
+          line = line.trim();
+          if (line.startsWith('###')) {
+            tt[1].push(line.replace(/#/g, '').trim());
+          } else if (line.startsWith('##')) {
+            if (tt.length > 0) {
+              this.tableOfTitles.push(tt);
+            }
+            tt = [line.replace(/#/g, '').trim(), []];
+          }
         });
-        const subtitleLists = this.subtitleLists;
-        fileMd.match(reg2).forEach(function (titleH3) {
-          subtitleLists.push(titleH3.toString().replace('###', ''))
-        });
-      })
+        if (tt.length > 0) {
+          this.tableOfTitles.push(tt);
+        }
+      });
     });
   }
-   getAnchor(event) {
-    event.preventDefault();
-     if (event.target) {
-       const reg3 = new RegExp('^()[A-Za-z 1-9]+', 'gm');
-       event.target.forEach(function (item) {
-         console.log(item);
-       });
-
-       console.log(this.currentAnchorId);
-     }
-   }
 }
+/* let previousLine = '';
+        for(let i=0; i < lines.length; i++) {
+          const line = lines[i].trim();
+          if (line.startsWith('###')) {
+            this.listsTable[previousLine].push(line.replace('###', '').trim());
+          } else if (line.startsWith('##')) { 
+            this.listsTable[line] = [];
+            previousLine = line;
+          }
+        }
+        console.log(this.listsTable); */
