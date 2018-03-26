235de5d7bde9cae73b6a0e6e78b6d82c5551eb21
Kevin Beyrand <kevin@atnos.com>
2018-03-26 16:13:48 +0200

    wip trello#281

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -27,7 +27,28 @@ export class SettingsComponent implements OnInit {
    * @memberof SettingsComponent
    */
   onSubmit() {
-    localStorage.setItem('server_url', this.settingsForm.value.server_url);
-    this._modalsService.openModal('modal-update-server-url');
+    const HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+      const anHttpRequest = new XMLHttpRequest();
+      anHttpRequest.onreadystatechange = function() {
+        if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200) {
+          aCallback(anHttpRequest.responseText);
+        }
+      }
+      anHttpRequest.open( 'GET', aUrl, true );
+      anHttpRequest.send( null );
+    }
+  }
+
+  const client = new HttpClient();
+  client.get('https://www.google.fr/', function(response) {
+      console.log(response);
+  });
+
+    // if ok
+      /*localStorage.setItem('server_url', this.settingsForm.value.server_url);
+      this._modalsService.openModal('modal-update-server-url-ok');*/
+    // else
+      // this._modalsService.openModal('modal-update-server-url-nok');
   }
 }
