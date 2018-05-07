d4590ad607450630ec74529cb237ac9d0193178f
Romain Sanchez <romainsancho@gmail.com>
2018-05-07 12:19:01 +0200

    Fix http query params format

--- src/api/service/base.service.ts
+++ src/api/service/base.service.ts
@@ -1,6 +1,6 @@
 
 import { BaseModel } from '@api/model/base.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import * as UrlTemplate from 'url-template';
 import { Injectable } from '@angular/core';
@@ -15,28 +15,38 @@ export class BaseService<T extends BaseModel> {
   constructor(protected http: HttpClient) { }
 
   protected httpGetAll(routeTpl: string, params: any = {}, query: any = {}): Observable<T[]> {
+    query = this.buildQuery(query);
     const route = this.buildRoute(routeTpl, params);
-    return this.http.get(route, {params: query}).map(res => this.mapToCollection(res, this.modelClass));
+
+    return this.http.get(route, query).map(res => this.mapToCollection(res, this.modelClass));
   }
 
   protected httpGetOne(routeTpl: string, params: any = {}, query: any = {}): Observable<T> {
+    query = this.buildQuery(query);
     const route = this.buildRoute(routeTpl, params);
+
     return this.http.get(route, {params: query}).map(res => this.mapToModel(res, this.modelClass));
   }
 
   protected httpPut(routeTpl: string, params: any = {}, model: T, query: any = {}): Observable<T> {
+    query = this.buildQuery(query);
     const route = this.buildRoute(routeTpl, params);
-    return this.http.put(route, model.toJson(), {params: query}).map(res => this.mapToModel(res, this.modelClass));
+
+    return this.http.put(route, model.toJson(), query).map(res => this.mapToModel(res, this.modelClass));
   }
 
   protected httpPost(routeTpl: string, params: any = {}, model: T, query: any = {}): Observable<T> {
+    query = this.buildQuery(query);
     const route = this.buildRoute(routeTpl, params);
-    return this.http.post(route, model.toJson(), {params: query}).map(res => this.mapToModel(res, this.modelClass));
+
+    return this.http.post(route, model.toJson(), query).map(res => this.mapToModel(res, this.modelClass));
   }
 
   protected httpDelete(routeTpl: string, params: any = {}, query: any = {}): Observable<T> {
+    query = this.buildQuery(query);
     const route = this.buildRoute(routeTpl, params);
-    return this.http.delete(route, {params: query}).map(res => this.mapToModel(res, this.modelClass));
+    
+    return this.http.delete(route, query).map(res => this.mapToModel(res, this.modelClass));
   }
 
 
@@ -51,5 +61,15 @@ export class BaseService<T extends BaseModel> {
 
   protected mapToCollection(res: any, modelClass: new () => T) {
     return res.map(item => (new modelClass()).fromJson(item));
+  }
+
+  protected buildQuery(query: any): HttpParams {
+    var params = new HttpParams();
+
+    for (let key in query) {
+      params = params.set(key, query[key]);
+    }
+
+    return params;
   }
 }
