5ba1f906fe275b42b693a0a50a24a339f6addac8
Romain Sanchez <romainsancho@gmail.com>
2018-05-31 11:41:23 +0200

    Remove id overriding in template model

--- src/api/model/template.model.ts
+++ src/api/model/template.model.ts
@@ -1,12 +1,9 @@
 import { BaseModel } from '@api/model/base.model'
 
 export class Template extends BaseModel {
-  public id: any;
   public enabled: boolean;
   public name: string;
   public description: string;
   public imported_file_name: string;
-  public created_at: any;
-  public updated_at: any;
 
 }
