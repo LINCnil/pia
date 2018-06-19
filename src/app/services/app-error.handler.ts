e072245986d3aa1d562609b9bf45706368cd819a
GlennCavarle <glenn.cavarle@libre-informatique.fr>
2018-06-19 11:37:30 +0200

    update error-handler to manage 401 and remove toastr border-radius

--- src/app/services/app-error.handler.ts
+++ src/app/services/app-error.handler.ts
@@ -1,11 +1,13 @@
-import { ErrorHandler, Inject, Injector, Injectable } from '@angular/core';
+import { ErrorHandler, Inject, Injector, Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import {TranslateService} from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthenticationService } from '@security/authentication.service'
 
 @Injectable()
 export class AppErrorHandler implements ErrorHandler {
 
-  constructor(private i18n:TranslateService, @Inject(Injector) private readonly injector: Injector) {
+  constructor(private i18n: TranslateService, @Inject(Injector) private readonly injector: Injector) {
 
   }
 
@@ -13,8 +15,22 @@ export class AppErrorHandler implements ErrorHandler {
     return this.injector.get(ToastrService);
   }
 
+  private get zone(): NgZone {
+    return this.injector.get(NgZone);
+  }
+
+  private get router(): Router {
+    return this.injector.get(Router);
+  }
+
+  private get auth(): AuthenticationService {
+    return this.injector.get(AuthenticationService);
+  }
+
+
+
   handleError(error: any) {
-    if(error.rejection == undefined){
+    if (error.rejection == undefined) {
       console.error("Unknown Error", error);
       return;
     }
@@ -22,17 +38,25 @@ export class AppErrorHandler implements ErrorHandler {
     let httpErrorCode = error.rejection.status;
 
     switch (httpErrorCode) {
+      case 401:
+        this.toastr.error(
+          null,
+          this.i18n.instant('messages.unauthorized.title')
+        );
+        this.auth.logout();
+        this.zone.run(()=>this.router.navigate([""]));
+        break;
       case 403:
         this.toastr.error(
           null,
-          this.i18n.instant('messages.unauthorized_action.title')
+          this.i18n.instant('messages.forbidden.title')
         );
 
         break;
       case 500:
         this.toastr.error(
           null,
-          this.i18n.instant('messages.server_error.title')
+          this.i18n.instant('messages.fatal_error.title')
         );
 
         break;
