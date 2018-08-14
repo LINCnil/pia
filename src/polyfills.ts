aaeda8b3dbdf6afc07515860cdf9ef8374d14f9d
PapsOu <vivien.frasca@gmail.com>
2018-08-14 14:35:06 +0200

    Fix polyfill missing global error

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -14,6 +14,8 @@
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
+(window as any).global = window;
+
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
