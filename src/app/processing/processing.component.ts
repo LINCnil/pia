0b7f0763cf26b36b28a492a673c1e210f38352a3
PapsOu <PapsOu@users.noreply.github.com>
2018-09-20 10:58:00 +0200

    Add PIA quick list on Processing form left sidebar (#76)
    * Add shortcut to create PIA from Processing form
    
    * Add permission check to display button
    
    * Add PIA quick list on processing form

--- src/app/processing/processing.component.ts
+++ src/app/processing/processing.component.ts
@@ -3,6 +3,11 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ProcessingModel } from '@api/models';
 import { ProcessingFormComponent } from './processing-form/processing-form.component';
+import { ModalsService } from '../modals/modals.service';
+import { PiaService } from '../entry/pia.service';
+import { PiaApi } from '@api/services';
+import { PiaModel } from '@api/models';
+import { PiaStatus } from '@api/model/pia.model';
 
 @Component({
   selector: 'app-processing',
@@ -14,14 +19,31 @@ export class ProcessingComponent implements OnInit {
   processing: ProcessingModel;
   sections: any;
   currentSection: Section;
+  pias: PiaModel[];
 
   constructor(
-    private route: ActivatedRoute
-  ) { }
+    private route: ActivatedRoute,
+    protected _modalsService: ModalsService,
+    private _piaService: PiaService,
+    private piaApi: PiaApi
+  ) {
+  }
 
   ngOnInit() {
     this.sections = this.route.snapshot.data.sections;
     this.processing = this.route.snapshot.data.processing;
+
+    this._piaService.currentProcessing = this.processing;
+
+    this.piaApi.getAll({'processing': this.processing.id}).subscribe((pias) => {
+      this.pias = pias.sort((a, b) => {
+        if (a.status > b.status) return 1;
+        if (a.status < b.status) return -1;
+        if (a.updated_at > b.updated_at) return -1;
+        if (a.updated_at < b.updated_at) return 1;
+        return 0;
+      });
+    });
 
     this.changeSection(1);
   }
