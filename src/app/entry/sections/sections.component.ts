884b9a867fab99463f0d6bd6ee6706e7d911fe59
Kevin Beyrand <kevin@atnos.com>
2019-01-08 16:28:19 +0100

    WIP on text when model outdated but problem encountered with updated_at for the model

--- src/app/entry/sections/sections.component.ts
+++ src/app/entry/sections/sections.component.ts
@@ -6,6 +6,7 @@ import { Evaluation } from 'app/entry/entry-content/evaluations/evaluation.model
 import { Measure } from 'app/entry/entry-content/measures/measure.model';
 import { Answer } from 'app/entry/entry-content/questions/answer.model';
 
+import { Structure } from 'app/structures/structure.model';
 import { AppDataService } from 'app/services/app-data.service';
 import { SidStatusService } from 'app/services/sid-status.service';
 import { PiaService } from 'app/services/pia.service';
@@ -22,6 +23,8 @@ export class SectionsComponent implements OnInit {
   @Input() section: { id: number, title: string, short_help: string, items: any };
   @Input() item: { id: number, title: string, evaluation_mode: string, short_help: string, questions: any };
   data: { sections: any };
+  structure: Structure = new Structure();
+  displayOutdatedStructureMessage: boolean;
 
   constructor(public _piaService: PiaService,
               private _appDataService: AppDataService,
@@ -30,6 +33,7 @@ export class SectionsComponent implements OnInit {
   }
 
   async ngOnInit() {
+    this.displayOutdatedStructureMessage = false;
     await this._piaService.getPIA();
     this.data = await this._appDataService.getDataNav();
     this.data.sections.forEach((section: any) => {
@@ -37,5 +41,14 @@ export class SectionsComponent implements OnInit {
         this._sidStatusService.setSidStatus(this._piaService, section, item);
       });
     });
+    if (this._piaService.pia.structure_name) {
+      // Check if there is a structure linked to a PIA
+      this.structure.get(this._piaService.pia.structure_id).then(() => {
+        // Display a special message if the structure "updated_at" date is > to the PIA "created_at" date
+          if (this.structure.updated_at > this._piaService.pia.created_at) {
+            this.displayOutdatedStructureMessage = true;
+          }
+        });
+    }
   }
 }
