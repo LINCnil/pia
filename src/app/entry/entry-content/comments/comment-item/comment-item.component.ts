1de157a9ca781c1f1bfebde43d5617767abc457f
Kosmas Schütz <kosmas.schuetz@gmx.com>
2019-06-30 21:12:18 +0200

    Internationalization of dates: Format dates in i18n shortDate format

--- src/app/entry/entry-content/comments/comment-item/comment-item.component.ts
+++ src/app/entry/entry-content/comments/comment-item/comment-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { LanguagesService } from 'src/app/services/languages.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-comment-item',
@@ -9,7 +11,8 @@ export class CommentItemComponent implements OnInit {
 
   @Input() comment: any;
 
-  constructor() { }
+  constructor(private _languagesService: LanguagesService,
+              private _translateService: TranslateService) { }
 
   ngOnInit() { }
 }
