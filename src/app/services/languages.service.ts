9a98f35b78dacfe43ec214edc7a4953ca24c8cb7
Kosmas Schütz <kosmas.schuetz@gmx.com>
2019-12-01 14:19:42 +0100

    refactor(languge-service): added swedish

--- src/app/services/languages.service.ts
+++ src/app/services/languages.service.ts
@@ -1,32 +1,58 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable()
 export class LanguagesService {
   selectedLanguage: string;
 
-  constructor(private _translateService: TranslateService) { }
+  constructor(private _translateService: TranslateService) {}
 
   /**
    * Initialize languages on the PIA tool
    */
   initLanguages() {
-    this._translateService.addLangs(['en', 'cz', 'de', 'dk', 'el', 'es', 'et', 'fi', 'fr', 'hr', 'hu', 'it', 'lt', 'nl', 'no', 'pl', 'pt', 'ro', 'sl']);
-    this._translateService.setDefaultLang('fr');
+    this._translateService.addLangs([
+      "en",
+      "cz",
+      "de",
+      "dk",
+      "el",
+      "es",
+      "et",
+      "fi",
+      "fr",
+      "hr",
+      "hu",
+      "it",
+      "lt",
+      "nl",
+      "no",
+      "pl",
+      "pt",
+      "ro",
+      "sl",
+      "sv"
+    ]);
+    this._translateService.setDefaultLang("fr");
   }
 
   /**
    * Get the current language or set it
    */
   getOrSetCurrentLanguage() {
-    let language = localStorage.getItem('userLanguage');
+    let language = localStorage.getItem("userLanguage");
     // If there is already a language choosen
     if (language && language.length > 0) {
       this._translateService.use(language);
-    } else { // Set default language
+    } else {
+      // Set default language
       const browserLang = this._translateService.getBrowserLang();
-      language = browserLang.match(/en|cz|de|dk|el|es|et|fi|fr|hr|hu|it|lt|nl|no|pl|pt|ro|sl/) ? browserLang : 'fr';
+      language = browserLang.match(
+        /en|cz|de|dk|el|es|et|fi|fr|hr|hu|it|lt|nl|no|pl|pt|ro|sl|sv/
+      )
+        ? browserLang
+        : "fr";
       this._translateService.use(language);
     }
     this.selectedLanguage = language;
@@ -37,7 +63,7 @@ export class LanguagesService {
    * @param {string} selectedLanguage
    */
   updateCurrentLanguage(selectedLanguage: string) {
-    localStorage.setItem('userLanguage', selectedLanguage);
+    localStorage.setItem("userLanguage", selectedLanguage);
     this._translateService.use(selectedLanguage);
     this.selectedLanguage = selectedLanguage;
   }
