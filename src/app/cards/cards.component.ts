1ce07fa2f8363bbb97b6a4b8efb5147e34767210
sylvain <sylvain@atnos.com>
2020-04-15 11:41:39 +0200

    feat: add introjs to project

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -10,13 +10,14 @@ import { ModalsService } from 'src/app/modals/modals.service';
 import { PiaService } from 'src/app/services/pia.service';
 import { StructureService } from 'src/app/services/structure.service';
 
+import * as introJs from 'intro.js/intro.js';
+
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss'],
   providers: [PiaService, StructureService]
 })
-
 export class CardsComponent implements OnInit, OnDestroy {
   @Input() pia: any;
   newPia: Pia;
@@ -29,12 +30,14 @@ export class CardsComponent implements OnInit, OnDestroy {
   paramsSubscribe: Subscription;
   searchText: string;
 
-  constructor(private router: Router,
-              private el: ElementRef,
-              private route: ActivatedRoute,
-              public _modalsService: ModalsService,
-              public _piaService: PiaService,
-              public _structureService: StructureService) { }
+  constructor(
+    private router: Router,
+    private el: ElementRef,
+    private route: ActivatedRoute,
+    public _modalsService: ModalsService,
+    public _piaService: PiaService,
+    public _structureService: StructureService
+  ) {}
 
   ngOnInit() {
     const structure = new Structure();
@@ -62,11 +65,9 @@ export class CardsComponent implements OnInit, OnDestroy {
     this.viewStyle = {
       view: this.route.snapshot.params.view
     };
-    this.paramsSubscribe = this.route.params.subscribe(
-      (params: Params) => {
-        this.viewStyle.view = params.view;
-      }
-    );
+    this.paramsSubscribe = this.route.params.subscribe((params: Params) => {
+      this.viewStyle.view = params.view;
+    });
     if (localStorage.getItem('homepageDisplayMode') === 'list') {
       this.viewOnList();
     } else {
@@ -78,13 +79,15 @@ export class CardsComponent implements OnInit, OnDestroy {
   }
 
   onCleanSearch() {
-    this.searchText = "";
+    this.searchText = '';
   }
 
   ngOnDestroy() {
     this.paramsSubscribe.unsubscribe();
   }
-
+  ngAfterViewInit() {
+    introJs().start();
+  }
   /**
    * On PIA change.
    * @param {any} pia - Any PIA.
@@ -92,8 +95,7 @@ export class CardsComponent implements OnInit, OnDestroy {
   piaChange(pia) {
     if (this._piaService.pias.includes(pia)) {
       this._piaService.pias.forEach(item => {
-        if (item.id === pia.id)
-        item = pia;
+        if (item.id === pia.id) item = pia;
       });
     } else {
       this._piaService.pias.push(pia);
@@ -202,8 +204,12 @@ export class CardsComponent implements OnInit, OnDestroy {
         firstValue = new Date(a[this.sortValue]);
         secondValue = new Date(b[this.sortValue]);
       }
-      if (this.sortValue === 'name' || this.sortValue === 'author_name' ||
-          this.sortValue === 'evaluator_name' || this.sortValue === 'validator_name') {
+      if (
+        this.sortValue === 'name' ||
+        this.sortValue === 'author_name' ||
+        this.sortValue === 'evaluator_name' ||
+        this.sortValue === 'validator_name'
+      ) {
         return firstValue.localeCompare(secondValue);
       } else {
         if (firstValue < secondValue) {
