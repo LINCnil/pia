d8d2972efaabd7fdae4454553720111ed7bfe3e0
sylvain <sylvain@atnos.com>
2020-04-29 11:37:21 +0200

    feat: restart onboarding for evaluation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -71,6 +71,7 @@ export class HeaderComponent implements OnInit {
    * Restart the onboarding module for the current user
    */
   restartOnboarding() {
+    localStorage.removeItem('onboardingEvaluationConfirmed');
     localStorage.removeItem('onboardingDashboardConfirmed');
     localStorage.removeItem('onboardingEntryConfirmed');
     location.reload();
