96102ccc4b33835d29cbe98788100a5e0ba9f2f3
sylv-p <sylvain@atnos.com>
2020-11-12 17:16:02 +0100

    feat: add preview example comp

--- src/app/modules/pia/pia-routing.module.ts
+++ src/app/modules/pia/pia-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 import { PiaComponent } from './pia.component';
+import { PreviewComponent } from './preview/preview.component';
 
 
 
 
 @NgModule({
   imports: [RouterModule.forChild([
+    { path: 'preview/:id', component: ExampleComponent },
     { path: 'pia/:id', component: PiaComponent },
     { path: 'pia/:id/section/:section_id/item/:item_id', component: PiaComponent },
   ])],
