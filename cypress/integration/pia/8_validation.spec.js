d79f13e3ac082eec46dd3b62501f15f663a14134
sylv-p <sylvain@atnos.com>
2020-12-16 10:09:07 +0100

    feat: import cypress old test and cypress e2e testing

---
+++ cypress/integration/pia/8_validation.spec.js
@@ -0,0 +1,22 @@
+describe('Validation', () => {
+  context('Avis du DPD et des personnes concernées', () => {
+    it('should complete DPD', () => {
+      cy.validateDPO();
+    });
+    it('should valid pia', () => {
+      cy.validatePia();
+      cy.closeValidationEvaluationModal();
+    });
+    it('should show report', () => {
+      cy.get('.pia-entryContentBlock-footer-validationTools > [href="#/preview/2"]').click();
+      cy.get('.fa-chevron-left').click();
+      cy.validatePia();
+    });
+    it('should show report and plan action download', () => {
+      cy.get('.pia-entryContentBlock-footer-validationTools > [href="#/preview/2').click();
+      cy.get('angular2csv').click();
+      cy.get('.fa-chevron-left').click();
+      cy.validatePia();
+    });
+  });
+});
