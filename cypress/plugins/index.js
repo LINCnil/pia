0d232cc9b2d7d2d118e548fe134093cdf420464e
sylvain-pastor <sylvain@atnos.com>
2021-01-07 17:18:27 +0100

    fix: change browser configuration on start

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,8 +13,30 @@
 
 module.exports = (on, config) => {
   on("before:browser:launch", (browser, launchOptions) => {
+    const downloadDirectory = "../download";
+    // CHROME
     if (browser.family === "chromium" && browser.name !== "electron") {
+      launchOptions.preferences.default["download"] = {
+        default_directory: downloadDirectory,
+        prompt_for_download: false
+      };
+
       launchOptions.preferences.default.intl = { accept_languages: "en" };
+      return launchOptions;
+    }
+
+    // FIREFOX
+    if (browser.family === "firefox") {
+      launchOptions.preferences["browser.download.dir"] = downloadDirectory;
+      launchOptions.preferences["browser.download.folderList"] = 2;
+      launchOptions.preferences["browser.download.promptMaxAttempts"] = 0;
+      launchOptions.preferences["browser.download.forbid_open_with"] = true;
+      launchOptions.preferences["browser.download.manager.useWindow"] = false;
+
+      // needed to prevent download prompt for text/csv files.
+      launchOptions.preferences["browser.helperApps.neverAsk.saveToDisk"] =
+        "text/csv";
+
       return launchOptions;
     }
   });
