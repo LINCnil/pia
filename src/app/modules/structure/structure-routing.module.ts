8908264fbe3b2bcccddb32c989ba6efecdeb1a63
sylv-p <sylvain@atnos.com>
2021-05-11 16:57:22 +0200

    feat: update structures for users features

--- src/app/modules/structure/structure-routing.module.ts
+++ src/app/modules/structure/structure-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/shared/guards/auth.guards';
 import { StructureComponent } from './structure.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'structures/:id', component: StructureComponent },
+      {
+        path: 'structures/:id',
+        component: StructureComponent,
+        canActivate: [AuthGuard]
+      },
       {
         path: 'structures/:id/section/:section_id/item/:item_id',
-        component: StructureComponent
+        component: StructureComponent,
+        canActivate: [AuthGuard]
       }
     ])
   ],
