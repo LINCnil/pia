3e6a3f8fe37749332379fc4e8b183244dd4f71b1
sylv-p <sylvain@atnos.com>
2021-05-17 11:42:58 +0200

    feat: add electron compiler in repo

---
+++ updater.js
@@ -0,0 +1,69 @@
+const { dialog, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+const path = require("path");
+const url = require("url");
+
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+autoUpdater.autoDownload = false;
+
+exports.check = () => {
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-available", () => {
+    dialog.showMessageBox(
+      {
+        type: "info",
+        title: "Update Available",
+        message:
+          "A new version of PIA is available. Do you want to update now?",
+        buttons: ["Update", "No"]
+      },
+      buttonIndex => {
+        if (buttonIndex !== 0) return;
+
+        autoUpdater.downloadUpdate();
+
+        let progressWin = new BrowserWindow({
+          width: 350,
+          height: 35,
+          useContentSize: true,
+          autoHideMenuBar: true,
+          maximizable: false,
+          fullscreen: false,
+          fullscreenable: false,
+          resizable: false
+        });
+
+        progressWin.loadURL(
+          url.format({
+            pathname: path.join(__dirname, "renderer", "progress.html"),
+            protocol: "file:",
+            slashes: true
+          })
+        );
+
+        progressWin.on("closed", () => {
+          progressWin = null;
+        });
+
+        autoUpdater.on("update-downloaded", () => {
+          if (progressWin) progressWin.close();
+          dialog.showMessageBox(
+            {
+              type: "info",
+              title: "Update ready",
+              message: "A new version of PIA is ready. Quit and install now?",
+              buttons: ["Yes", "Later"]
+            },
+            buttonIndex => {
+              if (buttonIndex === 0) autoUpdater.quitAndInstall();
+            }
+          );
+        });
+      }
+    );
+  });
+};
