bdb38b627c2fe4c5bf0decc02427f28cb6250f37
BEYRAND Kévin <kevin@atnos.com>
2021-08-23 11:44:33 +0200

    feat: add lv (letton) to languages

--- src/app/services/languages.service.ts
+++ src/app/services/languages.service.ts
@@ -6,27 +6,54 @@ import { TranslateService } from '@ngx-translate/core';
 export class LanguagesService {
   selectedLanguage: string;
 
-  constructor(private translateService: TranslateService) { }
+  constructor(private translateService: TranslateService) {}
 
   /**
    * Initialize languages on the PIA tool
    */
   initLanguages(): void {
-    this.translateService.addLangs(['en', 'cz', 'de', 'dk', 'el', 'es', 'et', 'fi', 'fr', 'hr', 'hu', 'it', 'lt', 'nl', 'no', 'pl', 'pt', 'ro', 'sl', 'sv']);
+    this.translateService.addLangs([
+      'en',
+      'cz',
+      'de',
+      'dk',
+      'el',
+      'es',
+      'et',
+      'fi',
+      'fr',
+      'hr',
+      'hu',
+      'it',
+      'lt',
+      'nl',
+      'no',
+      'pl',
+      'pt',
+      'ro',
+      'sl',
+      'sv',
+      'lv'
+    ]);
     this.translateService.setDefaultLang('fr');
   }
 
   /**
    * Get the current language or set it
    */
-  getOrSetCurrentLanguage(): void  {
+  getOrSetCurrentLanguage(): void {
     let language = localStorage.getItem('userLanguage');
     // If there is already a language choosen
     if (language && language.length > 0) {
       this.translateService.use(language);
-    } else { // Set default language
+    } else {
+      // Set default language
       const browserLang = this.translateService.getBrowserLang();
-      language = browserLang.match(/en|cz|de|dk|el|es|et|fi|fr|hr|hu|it|lt|nl|no|pl|pt|ro|sl|sv/) ? browserLang : 'fr';
+      language = browserLang.match(
+        /en|cz|de|dk|el|es|et|fi|fr|hr|hu|it|lt|nl|no|pl|pt|ro|sl|sv|lv/
+      )
+        ? browserLang
+        : 'fr';
       this.translateService.use(language);
     }
     this.selectedLanguage = language;
@@ -36,7 +63,7 @@ export class LanguagesService {
    * Update the current language after choosing a new one
    * @param {string} selectedLanguage
    */
-  updateCurrentLanguage(selectedLanguage: string): void  {
+  updateCurrentLanguage(selectedLanguage: string): void {
     localStorage.setItem('userLanguage', selectedLanguage);
     this.translateService.use(selectedLanguage);
     this.selectedLanguage = selectedLanguage;
