f426dcba517d8bbc5e4616e1304d2b209fd5b542
syl-p <sylvain@atnos.com>
2023-04-03 10:34:41 +0200

    feat: dynamic version number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import * as packageJson from '../../package.json';
 
 export const environment = {
   name: 'development',
   production: false,
-  version: 'de DEV'
+  version: packageJson.version
 };
 
 /*
