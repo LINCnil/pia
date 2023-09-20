bd377a6a2f0240f2ead22b3f94161b083d004d2a
Sylvain Pastor <sylvain@mbpdeatnosadmin.home>
2023-09-20 15:53:37 +0200

    chore: update all cypress configuration + fixes some test

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  // projectId: 'w4expe',
+  viewportWidth: 1200,
+  viewportHeight: 660,
+  // experimentalSourceRewriting: false,
+  // trashAssetsBeforeRuns: true,
+  downloadsFolder: './cypress/download',
+  defaultCommandTimeout: 10000,
+  // video: false,
+  retries: 5,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
+  }
+});
