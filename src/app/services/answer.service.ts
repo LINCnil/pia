7134924edb3258bc83976139d6ef040c3577fd58
syl-p <sylvain@atnos.com>
2024-09-09 16:44:53 +0200

    fix: fix async, fix not founded pia_id in applicationdb

--- src/app/services/answer.service.ts
+++ src/app/services/answer.service.ts
@@ -102,9 +102,7 @@ export class AnswerService extends ApplicationDb {
   }
 
   async findAllByPia(pia_id: number): Promise<any> {
-    const items = [];
     return new Promise((resolve, reject) => {
-      this.pia_id = pia_id;
       super
         .findAll(null, { index: 'index2', value: pia_id })
         .then((result: any) => {
