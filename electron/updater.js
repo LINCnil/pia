de9f75850e7b885de717213cb63e18e479847163
Bruno Perles <bruno@atnos.com>
2025-06-09 21:51:34 +0200

    feat: add progress window for update downloads

--- electron/updater.js
+++ electron/updater.js
@@ -1,5 +1,36 @@
+const { BrowserWindow } = require("electron/main");
 const { autoUpdater } = require("electron-updater");
 const { dialog } = require("electron");
+const path = require("path");
+
+let progressWindow = null;
+
+function createProgressWindow() {
+  // Create a new window for the progress bar
+  progressWindow = new BrowserWindow({
+    width: 400,
+    height: 150,
+    useContentSize: true,
+    resizable: false,
+    minimizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    title: "Downloading Update",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load progress bar HTML file
+  progressWindow.loadFile(path.join(__dirname, "progress.html"));
+  progressWindow.setMenuBarVisibility(false);
+  progressWindow.on("closed", () => {
+    progressWindow = null;
+  });
+
+  return progressWindow;
+}
 
 function setupAutoUpdater() {
   // Configure autoUpdater - disable automatic download and install
@@ -33,10 +64,23 @@ function setupAutoUpdater() {
 
   autoUpdater.on("download-progress", progress => {
     console.log(`Download progress: ${progress.percent.toFixed(2)}%`);
+    if (!progressWindow) {
+      createProgressWindow();
+    }
+
+    if (progressWindow) {
+      progressWindow.webContents.send("download-progress", {
+        percent: progress.percent.toFixed(2)
+      });
+    }
   });
 
   autoUpdater.on("update-downloaded", info => {
     console.log("Update downloaded", info);
+    if (progressWindow) {
+      progressWindow.close();
+      progressWindow = null;
+    }
     dialog
       .showMessageBox({
         type: "info",
@@ -51,6 +95,10 @@ function setupAutoUpdater() {
 
   autoUpdater.on("error", err => {
     console.error("AutoUpdater error:", err);
+    if (progressWindow) {
+      progressWindow.close();
+      progressWindow = null;
+    }
   });
 
   // Check for updates
