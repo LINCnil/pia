80e0c87e684974f44de0661c9ca633d0a213bfb1
Bruno Perles <bruno@atnos.com>
2025-09-25 21:43:55 +0200

    fix: cypress test for file input + cypress envs

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,10 +13,16 @@ export default defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    // tslint:disable-next-line:typedef
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
+  },
+  env: {
+    URL: 'localhost:3000',
+    ID: 'client_id',
+    SECRET: 'client_secret'
   }
 });
